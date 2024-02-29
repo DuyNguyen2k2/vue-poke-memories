9c4747916d54fcc3a2fb58db9d56f445341f6bc5
DuyNguyen2k2 <duynguyen.s2k2@gmail.com>
2024-02-29 13:06:19 +0700

    feat(game) tao 1 mini game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="onStartAgain"
+  />
+  <copy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array.js";
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRight.vue";
+// function randomImages(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRight,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running handle before start", config);
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+      const allNumbers = Array.from({ length: 64 }, (_, i) => i + 1);
+
+      // Trộn lẫn mảng các số
+      const shuffledNumbers = shuffled(allNumbers);
+
+      // Lấy N/2 phần tử đầu tiên của mảng đã trộn để tạo danh sách số không trùng nhau
+      const uniqueNumbers = shuffledNumbers.slice(
+        0,
+        this.settings.totalOfBlocks / 2
+      );
+
+      // Tạo mảng chứa các số ngẫu nhiên từ danh sách uniqueNumbers
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => uniqueNumbers[i]
+      );
+
+      const secondCards = [...firstCards];
+      console.log(firstCards);
+      console.log(secondCards);
+      const cards = [...firstCards, ...secondCards];
+      console.log(cards);
+      this.settings.cardsContext = shuffled(
+        shuffled(shuffled(shuffled(cards)))
+      );
+      this.settings.startedAt = new Date().getTime();
+      console.log(this.settings.cardsContext);
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      this.timer = new Date().getTime() - this.settings.startedAt;
+      this.statusMatch = "result";
+    },
+    onStartAgain() {
+      this.statusMatch = "default"; // Đặt lại trạng thái về "default"
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
